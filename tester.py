19373b25e86e2bc94223ffce9c787bf9f2f2af43
DevinSuryadi <devinsurya95@gmail.com>
2025-02-22 16:22:43 +0700

    Perbaiki yang tester.py

--- tester.py
+++ tester.py
@@ -1,13 +1,12 @@
-import os
 import network
 import urequests as request
 import dht
-from machine import Pin, ADC
+from machine import Pin
 import utime as time
 
-# Connect to Wi-Fi
-ssid = os.getenv("ssid1")
-password = os.getenv("password")
+# Koneksi ke Wi-Fi
+ssid = "WISMA BERIMAN 3"
+password = "ALVIN0713"
 
 wifi = network.WLAN(network.STA_IF)
 wifi.active(True)
@@ -16,38 +15,28 @@ wifi.connect(ssid, password)
 while not wifi.isconnected():
     pass
 
-print("Connected to Wi-Fi!")
+print("Connected to WiFi!")
 
-# Initiate DHT11 sensor on GPIO19
+# Inisialisasi sensor DHT11 di GPIO4
 sensor = dht.DHT11(Pin(19))
-
-# Initiate Light Sensor (LDR) on GPIO34 (ADC1)
-light_sensor = ADC(Pin(34))
-light_sensor.atten(ADC.ATTN_11DB)  # Set ADC range to 0-3.3V (ESP32)
+pir = Pin(14, Pin.IN)
 
 while True:
     try:
         sensor.measure()
         temp = sensor.temperature()
         hum = sensor.humidity()
-        
-        # Read light intensity (0-4095 for ESP32)
-        light_value = light_sensor.read()
-        
-        # Convert ADC value to percentage (0-100%)
-        light_percentage = (light_value / 4095) * 100
-        
-        data = {
-            "temperature": temp,
-            "humidity": hum,
-            "light_intensity": light_percentage
-        }
+        if (pir.value() == 1):
+            motion = "Motion Detected!"
+        else:
+            motion = "No Motion"
+        data = {"temperature": temp, "humidity": hum, "Motion": motion}
 
-        # Send data to Flask server
+        # Kirim data ke server Flask
         response = request.post("http://192.168.1.52:7000/data", json=data)
         print(response.text)
         response.close()
     except Exception as e:
         print("Error:", e)
-    
     time.sleep(7)
+
